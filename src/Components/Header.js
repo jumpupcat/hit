7ba83b09c1bae3f2e0c7d801d7180baa00bd9747
jumpupcat <jumpupmoon@gmail.com>
2023-12-29 00:28:41 +0900

    soldout

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,85 +1,85 @@
-import React, { useState, useEffect } from "react";
-import axios from 'axios';
+// import React, { useState, useEffect } from "react";
+// import axios from 'axios';
 import ParticlesBg from "particles-bg";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import ProgressBar from "@ramonak/react-progress-bar";
+// import ProgressBar from "@ramonak/react-progress-bar";
 
 export default function Header() {
-  const [supply, setSupply] = useState(0);
+  // const [supply, setSupply] = useState(0);
 
-  const tokenAddr = 'cxd8303731bf989a1597a6d38505d7a9483c4d9a38'
-  const scan = `https://scan.havah.io/token/${tokenAddr}`;
-  const twitter = 'https://twitter.com/godhavah';
+  // const tokenAddr = 'cxd8303731bf989a1597a6d38505d7a9483c4d9a38'
+  // const scan = `https://scan.havah.io/token/${tokenAddr}`;
+  // const twitter = 'https://twitter.com/godhavah';
 
-  useEffect(() => {
-    axios.post('https://ctz.havah.io/api/v3', {
-      "jsonrpc":"2.0",
-      "id":21000000,
-      "method":"icx_call",
-      "params":{
-        "to":"cxd8303731bf989a1597a6d38505d7a9483c4d9a38",
-        "dataType":"call",
-        "data":{
-          "method":"totalSupply"
-        }
-      }
-    })
-    .then(({ data }) => {
-      if(data && data?.result) {
-        const supply = parseInt(data.result, 16);
-        setSupply(supply / 1000000);
-      }
-    });
-  }, []); 
+  // useEffect(() => {
+  //   axios.post('https://ctz.havah.io/api/v3', {
+  //     "jsonrpc":"2.0",
+  //     "id":21000000,
+  //     "method":"icx_call",
+  //     "params":{
+  //       "to":"cxd8303731bf989a1597a6d38505d7a9483c4d9a38",
+  //       "dataType":"call",
+  //       "data":{
+  //         "method":"totalSupply"
+  //       }
+  //     }
+  //   })
+  //   .then(({ data }) => {
+  //     if(data && data?.result) {
+  //       const supply = parseInt(data.result, 16);
+  //       setSupply(supply / 1000000);
+  //     }
+  //   });
+  // }, []); 
 
-  const mint = () => {
-    if(supply >= 21_000_000) {
-      toast.error("$GODH SOLD OUT");
-      return
-    }
+  // const mint = () => {
+  //   if(supply >= 21_000_000) {
+  //     toast.error("$GODH SOLD OUT");
+  //     return
+  //   }
 
-    if(!window.havah) {
-      toast.error("HAVAH wallet is not installed");
-      return;
-    }
+  //   if(!window.havah) {
+  //     toast.error("HAVAH wallet is not installed");
+  //     return;
+  //   }
 
-    window.havah.connect()
-    .then(res => {
-      let nid;
-      if(res?.nid) {
-        nid = res.nid;
-      } else if(res?.body?.nid) {
-        nid = res.body.nid;
-      }
+  //   window.havah.connect()
+  //   .then(res => {
+  //     let nid;
+  //     if(res?.nid) {
+  //       nid = res.nid;
+  //     } else if(res?.body?.nid) {
+  //       nid = res.body.nid;
+  //     }
       
-      if(nid !== '0x100') {
-        toast.error("wrong network, mint only mainnet");
-        return;
-      }
+  //     if(nid !== '0x100') {
+  //       toast.error("wrong network, mint only mainnet");
+  //       return;
+  //     }
       
-      const transactionData = {
-        to: tokenAddr,
-        method: 'mint'
-      };
+  //     const transactionData = {
+  //       to: tokenAddr,
+  //       method: 'mint'
+  //     };
       
-      window.havah.sendTransaction(transactionData)
-      .then(({ type }) => {
-        if(type === 'success') {
-          toast.success("mint success");
-          setSupply(prev => prev+1);
-        }
-      })
-      .catch(error => {
-        toast.error("tx send error, try again");
-        console.error(`Error: ${error}`)
-      });
-    })
-    .catch(error => {
-      toast.error("wallet connect error, try again");
-      console.error(`Error: ${error}`)
-    });
-  }
+  //     window.havah.sendTransaction(transactionData)
+  //     .then(({ type }) => {
+  //       if(type === 'success') {
+  //         toast.success("mint success");
+  //         setSupply(prev => prev+1);
+  //       }
+  //     })
+  //     .catch(error => {
+  //       toast.error("tx send error, try again");
+  //       console.error(`Error: ${error}`)
+  //     });
+  //   })
+  //   .catch(error => {
+  //     toast.error("wallet connect error, try again");
+  //     console.error(`Error: ${error}`)
+  //   });
+  // }
 
   return (
     <header id="home">
@@ -94,12 +94,9 @@ export default function Header() {
               Total Supply 21,000,000<br />
               Only need to pay gas fee<br />
               No roadmap No utility No owner<br />
-              1 MINT = 1 $GODH<br />
-              <br />
-              { supply.toLocaleString() }/21,000,000
             </h3>
 
-            <ProgressBar completed={Math.round(supply/21000000*100)} />
+            {/* <ProgressBar completed={Math.round(supply/21000000*100)} />
             <hr />
             
             <ul className="social">
@@ -112,7 +109,7 @@ export default function Header() {
               <a href={twitter} className="button btn twitter-btn" target="_blank" rel="noreferrer">
                 TWITTER
               </a>
-            </ul>
+            </ul> */}
         </div>
       </div>
 
